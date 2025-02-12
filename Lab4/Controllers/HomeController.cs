db10e83eb571373aff8d6745f53692e945fdf488
Yen Huynh Hoai <peterhuynh.contact@gmail.com>
2025-02-12 11:39:52 -0500

    Updated lab 4

---
+++ Lab4/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lab4.Models;
+
+namespace Lab4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SongForm() => View();
+
+        [HttpPost]
+        public IActionResult Sing(int monkeys)
+        {
+            ViewData["monkeys"] = monkeys;
+            return View();
+        }
+
+        public IActionResult CreateStudent() => View();
+
+        [HttpPost]
+        public IActionResult DisplayStudent(Student student)
+        {
+            return View(student);
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Lab4/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab4.Models
+{
+    public class Student
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public int StudentID { get; set; }
+
+        public string EmailAddress { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
